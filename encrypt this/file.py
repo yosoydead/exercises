59b23cc97b7a0712969d373f2107d7ef3c85c15c
yosoydead <radu-bogdan@outlook.com>
2018-09-11 13:28:19 +0300

    problem: encrypt this

---
+++ encrypt this/file.py
@@ -0,0 +1,28 @@
+
+#string = "Hello world this is a sample text"
+#string = "A wise old owl lived in an oak"
+string = "The more he saw the less he spoke"
+
+def x(string):
+    result = ""
+
+    for item in string.split():
+        wordsFirstLetter = ord(item[0])
+        if len(item) == 2:
+            currentWord = str(wordsFirstLetter) + item[1]
+            print(currentWord)
+            result += (currentWord + " ")
+        elif len(item) == 1:
+            print(wordsFirstLetter)
+            result += (wordsFirstLetter + " ")
+        else:
+            secondLetter = item[1]
+            lastLetter = item[len(item)-1]
+            theRest = item[2:len(item)-1]
+            bla = str(wordsFirstLetter) + lastLetter + theRest + secondLetter
+            print(bla)
+            result += (bla+ " ")
+    return result.strip()
+    #    print(item)
+
+print(x(string))
