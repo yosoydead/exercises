7b8019242438331385ff725de522caa76d2e2e5d
yosoydead <radu-bogdan@outlook.com>
2018-09-11 13:35:01 +0300

    added comments to problem: encrypt this

--- encrypt this/file.py
+++ encrypt this/file.py
@@ -5,24 +5,26 @@ string = "The more he saw the less he spoke"
 
 def x(string):
     result = ""
-
-    for item in string.split():
-        wordsFirstLetter = ord(item[0])
-        if len(item) == 2:
-            currentWord = str(wordsFirstLetter) + item[1]
-            print(currentWord)
-            result += (currentWord + " ")
-        elif len(item) == 1:
-            print(wordsFirstLetter)
-            result += (wordsFirstLetter + " ")
-        else:
-            secondLetter = item[1]
-            lastLetter = item[len(item)-1]
-            theRest = item[2:len(item)-1]
-            bla = str(wordsFirstLetter) + lastLetter + theRest + secondLetter
-            print(bla)
-            result += (bla+ " ")
-    return result.strip()
-    #    print(item)
+    if string == "":
+        return ""
+    else:
+        for item in string.split():
+            wordsFirstLetter = ord(item[0])
+            if len(item) == 2:
+                currentWord = str(wordsFirstLetter) + item[1]
+                print(currentWord)
+                result += (currentWord + " ")
+            elif len(item) == 1:
+                print(wordsFirstLetter)
+                result += (wordsFirstLetter + " ")
+            else:
+                secondLetter = item[1]
+                lastLetter = item[len(item)-1]
+                theRest = item[2:len(item)-1]
+                bla = str(wordsFirstLetter) + lastLetter + theRest + secondLetter
+                print(bla)
+                result += (bla+ " ")
+        return result.strip()
+        #    print(item)
 
 print(x(string))
