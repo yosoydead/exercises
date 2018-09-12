4e2fac1afab231af04adfe0c457d5b784c20a9ee
yosoydead <radu-bogdan@outlook.com>
2018-09-12 14:39:04 +0300

    problem: blood alcohol content. easy

---
+++ BloodAlcoholCalculator/BloodAlcoholCalculator/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputToBeNumbersVerify
+{
+    public interface IVerify
+    {
+        bool verify(string x);
+    }
+
+    class Verify : IVerify
+    {
+        public bool verify(string x)
+        {
+            if (string.IsNullOrEmpty(x) ||
+                string.IsNullOrWhiteSpace(x) ||
+                x.Any(char.IsLetter))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid input.");
+                Console.ResetColor();
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ BloodAlcoholCalculator/BloodAlcoholCalculator/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InputToBeNumbersVerify;
+
+namespace BloodAlcoholCalculator
+{
+    class Program
+    {
+        const double forMen = 0.73;
+        const double forWomen = 0.66;
+        const double bloodAlcoholMaxLvl = 0.08;
+
+        //custom class to check if the input is a number
+        static Verify vf = new Verify();
+
+        //ask for input and verify it
+        static string input()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            var l = Console.ReadLine();
+            while (vf.verify(l) == false)
+            {
+                l = Console.ReadLine();
+            }
+            Console.ResetColor();
+            return l;
+        }
+
+        //convert input to double
+        static double toNr(string x) => double.Parse(x);
+
+        //calculate the blood alcohol content
+        static double BAC (double A, double W, double r, double H)
+        {
+            var bac = ((A * 5.14) / (W * r)) - .015 * H;
+            return bac;
+        }
+
+        static void Main(string[] args)
+        {
+            int A, W, H;
+            double r = 0;
+            Console.WriteLine("Sex? M/F");
+            var sex = Console.ReadLine();
+
+            //update the alcohol distribution ratio 0.73 men, 0.66 women
+            if (sex == "M")
+            {
+                r = forMen;
+            }
+            else if (sex == "F")
+            {
+                r = forWomen;
+            }
+
+            Console.WriteLine("Alcohol consumed? ");
+            A = (int)toNr(input());
+
+            Console.WriteLine("How much time since the last drink ?");
+            H = (int)toNr(input());
+
+            Console.WriteLine("Body weight? ");
+            W = (int)toNr(input());
+
+            Console.WriteLine($"alcohol ratio {BAC(A, W, r, H)}");
+
+            if (BAC(A, W, r, H) > 0.08)
+            {
+                Console.WriteLine("Not legal");
+            }
+        }
+    }
+}
