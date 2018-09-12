d7b66f6859ecd797bbc019830351c004e5af1c90
yosoydead <radu-bogdan@outlook.com>
2018-09-12 13:21:08 +0300

    problem: find the unique number

---
+++ FindUniqueNumber/FindUniqueNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindUniqueNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //there will be only one number different from the others
+            int[] arr = new int[] { 1, 2, 2, 2 };
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //do the exclusive or operation so in the end only the
+                //number that is not equal to the other will remain
+                //if all the numbers were to be equal, result would remain 0
+                result ^= arr[i];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
