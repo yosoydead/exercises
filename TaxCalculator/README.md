b0afc9292bc718d0741f70ac97c13c7bbf767b82
yosoydead <radu-bogdan@outlook.com>
2018-09-12 14:19:16 +0300

    problem: tax calculator for Wi. easy

---
+++ TaxCalculator/README.md
@@ -0,0 +1,13 @@
+Write a simple program to compute the tax on an order
+        amount. The program should prompt for the order amount
+        and the state. If the state is “WI,” then the order must be
+        charged 5.5% tax. The program should display the subtotal,
+        tax, and total for Wisconsin residents but display just the
+        total for non-residents.
+
+        Constraints
+            • Implement this program using only a simple if statement—
+            don’t use an else clause.
+            • Ensure that all money is rounded up to the nearest cent.
+            • Use a single output statement at the end of the program
+            to display the program results.
---
+++ TaxCalculator/TaxCalculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InputToBeNumbersVerify;
+
+namespace TaxCalculator
+{
+    class Program
+    {
+        //custom class to check if the input is a number
+        static Verify vfNumer = new Verify();
+        const float tax = 5.5F;
+
+        //ask for input and verify it
+        static string input()
+        {
+            var l = Console.ReadLine();
+            while (vfNumer.verify(l) == false)
+            {
+                l = Console.ReadLine();
+            }
+            return l;
+        }
+
+        //convert the input into an int
+        static int transformToInt(string x)
+        {
+            int result = Int32.Parse(x);
+            return result;
+        }
+        
+        static void Main(string[] args)
+        {
+            int amount;
+            float total;
+            Console.Write("What is the order amount? ");
+            amount = transformToInt(input());
+            total = amount + (amount * (tax / 100));
+            Console.WriteLine("What is the state? ");
+            var state = Console.ReadLine();
+            if (state == "WI" || state == "wi" || state == "Wi")
+            {
+                Console.WriteLine($"Subtotal is {amount:c}, tax is {(tax/100):c}, total is {total:c}");
+            }
+            else
+            {
+                Console.WriteLine($"Total is {amount}");
+            }
+        }
+    }
+}
