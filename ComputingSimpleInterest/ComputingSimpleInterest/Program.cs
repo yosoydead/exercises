b317421d6405a877ef9935720d44df65fbf37423
yosoydead <radu-bogdan@outlook.com>
2018-09-12 14:43:30 +0300

    problem: computing simple interest. easy

---
+++ ComputingSimpleInterest/ComputingSimpleInterest/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InputToBeNumbersVerify;
+
+namespace ComputingSimpleInterest
+{
+    class Program
+    {
+        //custom class to check if the input is a number
+        static Verify vf = new Verify();
+        static string input()
+        {
+            var l = Console.ReadLine();
+            while (vf.verify(l) == false)
+            {
+                l = Console.ReadLine();
+            }
+            return l;
+        }
+
+        //convert the input into a double
+        static double convertToDouble()
+        {
+            double result = double.Parse(input());
+            return result;
+        }
+
+        //compute the simple interest
+        static double simpleInterest(double p, double r, double t)
+        {
+            //simple interest is A = P(1 + r*t)
+            //p = principal amount
+            //r = annual rate of interest
+            //t = number of years the amount is invested
+            double result = p*(1 + (r * t));
+            return result;
+        }
+
+        //percent convert
+        static float transformToPercent(string x)
+        {
+            float result = float.Parse(x) / 100;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the principal: ");
+            var principal = convertToDouble();
+            Console.WriteLine("Enter the rate of interest: ");
+            var x = Console.ReadLine();
+            var rate = transformToPercent(x);
+            Console.WriteLine("Enter the number of years: ");
+            var time = convertToDouble();
+            for (int i = 1; i <= time; i++)
+            {
+                Console.WriteLine($"Year {i}, rate {rate*100}, investment {simpleInterest(principal,rate,i)}");
+            }
+            Console.WriteLine($"After {time} years at {rate*100}%, the investment will be worth {simpleInterest(principal,rate,time):C}");
+        }
+    }
+}
---
+++ ComputingSimpleInterest/README.md
@@ -0,0 +1,28 @@
+Create a program that computes simple interest. Prompt for
+    the principal amount, the rate as a percentage, and the time,
+    and display the amount accrued (principal + interest).
+        
+    The formula for simple interest is A = P(1 + rt), where P is
+    the principal amount, r is the annual rate of interest, t is the
+    number of years the amount is invested, and A is the amount
+    at the end of the investment.
+        
+    Constraints
+        • Prompt for the rate as a percentage (like 15, not .15).
+        Divide the input by 100 in your program.
+        • Ensure that fractions of a cent are rounded up to the
+        next penny.
+        • Ensure that the output is formatted as money.
+
+    Challenges
+        • Ensure that the values entered for principal, rate, and
+        number of years are numeric and that the program will
+        not let the user proceed without valid inputs.
+        • Alter the program to use a function called calculateSimpleInterest
+        that takes in the rate, principal, and number
+        of years and returns the amount at the end of the
+        investment. -> done
+        • In addition to printing out the final amount, print out
+        the amount at the end of each year. -> done
+
+Simple problem. Only simple math involved.
