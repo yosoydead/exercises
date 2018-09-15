cdd59d1894e04a18627ae0eba3e918996ecfb60a
yosoydead <radu-bogdan@outlook.com>
2018-09-15 17:01:20 +0300

    problem: print staircase. easy

---
+++ staircase/staircase/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if(j >= 6-1-i)
+                        Console.Write("#");
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            
+        }
+
+
+    }
+}
