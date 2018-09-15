b355aa66d205b8dfa7960def382cebdd3d82c714
yosoydead <radu-bogdan@outlook.com>
2018-09-15 16:31:50 +0300

    problem: pizza party. easy

---
+++ PizzaParty/PizzaParty/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaParty
+{
+    class Program
+    {
+        //custom class to check if the input is a number
+        static bool verify(string x)
+        {
+            if (x.Any(char.IsLetter) ||
+                string.IsNullOrEmpty(x) ||
+                string.IsNullOrWhiteSpace(x) ||
+                x.Any(char.IsPunctuation) ||
+                int.Parse(x) <= 0)
+            {
+                Console.WriteLine("Input must not be a letter, string, punctuation or whitespace.");
+                return false;
+            }
+            return true;
+        }
+
+        static void pizzaSlicesToEveryone()
+        {
+            int people, pizzas, result;
+            int slices = 8;
+            float leftOvers;
+
+            Console.Write("How many people are coming to the party? ");
+            var ppl = Console.ReadLine();
+            while (verify(ppl) == false)
+            {
+                ppl = Console.ReadLine();
+            }
+
+            Console.Write("How many pizzas do you have? ");
+            var pieces = Console.ReadLine();
+            while (verify(pieces) == false)
+            {
+                pieces = Console.ReadLine();
+            }
+
+            people = Convert.ToInt32(ppl);
+            pizzas = int.Parse(pieces);
+            result = (pizzas * slices) / people;
+            leftOvers = (float)(pizzas * slices) % people;
+
+            Console.WriteLine($"So there are {ppl} persons and {pieces} pizzas. That means {pizzas * slices} slices. Let us see.");
+
+            if (result <= 1)
+            {
+                Console.WriteLine($"Each person gets {result} piece of pizza.");
+            }
+            else if (result >= 2)
+            {
+                Console.WriteLine($"Each person gets {result} pieces of pizza");
+            }
+            Console.WriteLine($"There are {leftOvers} leftover pieces.");
+        }
+
+        static void howManyFullPizzasYouNeed()
+        {
+            int slicesInPizza = 8;
+            int people, slices;
+            float fullPizzas;
+
+            Console.Write("How many people are coming to the party? ");
+            var ppl = Console.ReadLine();
+            while (verify(ppl) == false)
+            {
+                ppl = Console.ReadLine();
+            }
+
+            Console.Write("How many slices of pizza does everybody want? ");
+            var slice = Console.ReadLine();
+            while (verify(slice) == false)
+            {
+                slice = Console.ReadLine();
+            }
+
+            people = int.Parse(ppl);
+            slices = int.Parse(slice);
+
+            fullPizzas = (float)(people + slices) / slicesInPizza;
+
+            Console.WriteLine($"So {people} people are coming and all of you want a total of {slices} slices of pizza.");
+
+            Console.WriteLine($"This means you will need {fullPizzas} pizzas.");
+        }
+
+        static void Main(string[] args)
+        {
+            pizzaSlicesToEveryone();
+           // howManyFullPizzasYouNeed();
+        }
+    }
+}
---
+++ PizzaParty/README.md
@@ -0,0 +1,15 @@
+Write a program to evenly divide pizzas. Prompt for the number 
+of people, the number of pizzas and the number of slices per pizza.
+Ensure that the number of pieces comes out evenly. Display the number
+of pieces of pizza each person should get. If there are leftovers, show
+the number of leftover pieces.
+
+Challenge: revise the program to ensure that inputs are entered as numeric values.
+    
+Dont allow the user to proceed if the value entered is not numeric. -> done
+    - alter the output so it handles plurization properly -> done
+    -create a variant of the program that prompts for the number of people
+    and the number of pieces each person wants and calculate how many full pizzas
+    you need to purchase.
+
+I think the code is pretty readable and self-explanatory. Not that much programming involved, only some basic math.
