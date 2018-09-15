91dbcbf6b5eb430358a85750d0c8145554fd7b90
yosoydead <radu-bogdan@outlook.com>
2018-09-15 15:42:33 +0300

    problem: area of rectangular room. easy problem

---
+++ AreaOfARectangularRoom/AreaOfARectangularRoom/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfARectangularRoom
+{
+    class Program
+    {
+        const double toMeterFactor = 0.09290304;
+        const double toFeetFactor = 10.7639104;
+        static bool verify(string x)
+        {
+            if (string.IsNullOrEmpty(x) ||
+                string.IsNullOrWhiteSpace(x) ||
+                x.Any(char.IsLetter) ||
+                x.Any(char.IsPunctuation) ||
+                int.Parse(x) <= 1)
+            {
+                Console.WriteLine("Invalid input.");
+                return false;
+            }
+            return true;
+        }
+
+        static void feet()
+        {
+            int length, width, area;
+            Console.Write("What is the length of the room in feet? ");
+            var l = Console.ReadLine();
+            while (verify(l) == false)
+            {
+                l = Console.ReadLine();
+            }
+
+            Console.Write("What is the width of the room in feet? ");
+            var w = Console.ReadLine();
+            while (verify(w) == false)
+            {
+                w = Console.ReadLine();
+            }
+
+            length = Convert.ToInt32(l);
+            width = Convert.ToInt32(w);
+            area = length * width;
+            double areaInMeters = area * toMeterFactor;
+
+            Console.WriteLine($"The area is {area} feet or {areaInMeters} meters.");
+        }
+
+        static void meters()
+        {
+            int length, width, area;
+            Console.Write("What is the length of the room in meters? ");
+            var l = Console.ReadLine();
+            while (verify(l) == false)
+            {
+                l = Console.ReadLine();
+            }
+
+            Console.Write("What is the width of the room in meters? ");
+            var w = Console.ReadLine();
+            while (verify(w) == false)
+            {
+                w = Console.ReadLine();
+            }
+
+            length = Convert.ToInt32(l);
+            width = Convert.ToInt32(w);
+            area = length * width;
+            double areaInFeet = area * toFeetFactor;
+
+            Console.WriteLine($"The area is {area} meters or {areaInFeet} feet.");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate area a room area in feet or meters. What is your choice? ");
+            string input = Console.ReadLine();
+
+            if (input == "feet")
+            {
+                feet();
+            }else if(input == "meters")
+            {
+                meters();
+            }
+
+        }
+    }
+}
---
+++ AreaOfARectangularRoom/README.md
@@ -0,0 +1,14 @@
+Create a program that calculates the area of a room
+    Prompt the user for the length and width of the room
+    in feet. Then display the area in both square feet
+    and square meters.
+
+    Constraints: keep the calculations separate from the output
+        -use a constant to hold the conversion factor
+
+    Challenge: revise the program to ensure that inputs are
+    entered as numeric values. Dont allow the user to proceed
+    if the value entered is not numeric.
+        -create a new version of the program that allows you
+        to choose feet or meters for your inputs
+
