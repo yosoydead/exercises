2569db0d354bcc0f356deac37de6346ddb928033
yosoydead <radu-bogdan@outlook.com>
2018-12-02 18:36:42 +0200

    implemented the verify interface to the permutation thing

---
+++ permutations(my way)/permutations(my way)/IVerify.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace permutations_my_way_
+{
+    //just an interface that can be used to create other classes with different
+    //properties and ways to validate and display/save all the permutations
+    public interface IVerify
+    {
+        bool isValid(int[] array, int index);
+        bool isSolution(int[] array, int index);
+    }
+}
